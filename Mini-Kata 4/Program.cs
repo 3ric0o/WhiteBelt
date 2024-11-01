798cbb270e86c3e9129441813f9b185ad50faa29
Aergu <erik.witkowski@student.forsbergsskola.se>
2024-11-01 18:20:22 +0100

    mini kata 4

--- Mini-Kata 4/Program.cs
+++ Mini-Kata 4/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace Mini_Kata_4;
+﻿namespace WhiteBelt;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Spawning enemies using a for loop");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Enemy #{i+1} spawned!");
+        }
+        
+        Console.WriteLine("\nSpawning enemies using a while loop");
+        int enemies = 1;
+        while (enemies <= 3)
+        {
+            Console.WriteLine($"Enemy #{enemies} spawned!");
+            enemies++;
+        }
     }
 }
