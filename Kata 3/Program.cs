cf26305a9f3077396d32358c439a9d19f3f6f202
Aergu <erik.witkowski@student.forsbergsskola.se>
2024-11-01 18:56:11 +0100

    kata 3 and 4

--- Kata 3/Program.cs
+++ Kata 3/Program.cs
@@ -1,9 +1,59 @@
-﻿namespace Kata_3;
+﻿namespace WhiteBelt;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Random rnd = new Random();
+        int playerLuck = rnd.Next(0, 11);
+        bool validInput = false;
+
+        while (!validInput)
+        {
+            Console.WriteLine("Choose an option:\n" +
+                              "1. Open the chest\n" +
+                              "2. Leave the chest\n" +
+                              "3. Leave the area");
+            
+            if (int.TryParse(Console.ReadLine(), out int option))
+            {
+                validInput = true;
+
+                if (option == 1)
+                {
+                    Console.WriteLine("You try to open the chest...");
+                    Console.WriteLine($"Player luck: {playerLuck}");
+                    if (playerLuck > 7)
+                    {
+                        Console.WriteLine("Success! The chest contains a rare gem.");
+                    }
+                    else if (playerLuck >= 5)
+                    {
+                        Console.WriteLine("You found some Gold.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The chest was trapped!");
+                    }
+                }
+                else if (option == 2)
+                {
+                    Console.WriteLine("The Player continued on his journey leaving the chest behind.");
+                }
+                else if (option == 3)
+                {
+                    Console.WriteLine("You leave the area! The mystery was left unsolved.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option. Please choose again.");
+                    validInput = false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("That's not a valid integer. Please try again.");
+            }
+        }
     }
 }
--- Kata 4/Program.cs
+++ Kata 4/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Kata_4;
+﻿namespace WhiteBelt;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int waves = 3;
+        int enemies = 4;
+        
+        for (int i = 0; i < waves; i++)
+        {
+            Console.WriteLine($"Waves {i+1} is starting!");
+            Thread.Sleep(1000);
+            for (int j = 0; j < enemies; j++)
+            {
+                Console.WriteLine($"Enemy #{j +1} has spawned!");
+            }
+            Thread.Sleep(1000);
+            Console.WriteLine($"Waves {i+1} is Completed. Preparing for the next wave...\n");
+            Thread.Sleep(1000);
+        }
     }
 }
